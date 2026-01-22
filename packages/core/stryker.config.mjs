4f4e4a7175362ddb8e0858a751a1a08ebd9230d9
Ryan Milligan <rmilligan@tenzrhealth.com>
2026-01-21 17:04:37 -0800

    init stryker

---
+++ packages/core/stryker.config.mjs
@@ -0,0 +1,44 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
+  packageManager: 'pnpm',
+  reporters: ['html', 'clear-text', 'progress'],
+  testRunner: 'vitest',
+  testRunner_comment:
+    'Take a look at https://stryker-mutator.io/docs/stryker-js/vitest-runner for information about the vitest plugin.',
+  coverageAnalysis: 'perTest',
+  plugins: ['@stryker-mutator/vitest-runner'],
+
+  tempDirName: "stryker-tmp",
+  ignorePatterns: [
+    "stryker-tmp/**",
+  ],
+
+  htmlReporter: {
+    fileName: "reports/mutation/mutation.html",
+  },
+
+   mutate: [
+    "src/createActor.ts",
+  ],
+  
+  mutator: {
+    excludedMutations: [
+      // "StringLiteral",
+      "BlockStatement",
+    ],
+  },
+  ignoreStatic: true,
+  
+  timeoutFactor: 2,
+  timeoutMS: 3 * 60 * 1000,
+  
+  vitest: {
+    dir: './test',
+    related: true,
+  }
+   
+};
+export default config;
